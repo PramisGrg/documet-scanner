95e8fcf9f454cd7a40b110783391a3d518d820d9
PramisGrg <gurungpramis361@gmail.com>
2025-09-14 01:33:02 +0545

    eas configuration for remote apk

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import VisionCameraComponent from "@/component/camera";
+import { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function Index() {
+const ParentComponent = () => {
+  const [showCamera, setShowCamera] = useState(false);
+
+  const handleCapture = (photoPath: string) => {
+    console.log("Photo captured:", photoPath);
+    setShowCamera(false);
+  };
+
+  const handleClose = () => {
+    setShowCamera(false);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }}>
+      {showCamera ? (
+        <VisionCameraComponent
+          onClose={handleClose}
+          onCapture={handleCapture}
+        />
+      ) : (
+        <TouchableOpacity
+          style={{ borderColor: "red", borderWidth: 1, padding: 12 }}
+          onPress={() => setShowCamera(true)}
+        >
+          <Text>Open Camera</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
-}
+};
+
+export default ParentComponent;
